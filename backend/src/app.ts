932d5d4cca86ba20e5852d5bcabd7d3cfc2fa45e
Nikhil Gupta <100pranjalgupta@gmail.com>
2026-03-18 15:38:57 +0530

    setup the backend and folder structure basic

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import { Request,Response,Application } from "express";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+import morgan from 'morgan';
+
+const app:Application = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req:Request, res:Response) => {
+  res.send("Backend is running");
+});
+
+export default app;
