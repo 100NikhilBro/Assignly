6ad5dcab68f291aacfe5fd90dde247d47aea1a17
Nikhil Gupta <100pranjalgupta@gmail.com>
2026-03-22 01:16:19 +0530

    Update assignment.routes.ts

--- backend/src/modules/assignemnt/assignment.routes.ts
+++ backend/src/modules/assignemnt/assignment.routes.ts
@@ -9,10 +9,9 @@ import { createAssignmentSchema } from "../../schema/assignment.schema";
 
 const router = express.Router();
 
-// ✅ POST route - auth middleware already applied in index.ts
+
 router.post("/", validate(createAssignmentSchema), createAssignmentController);
 
-// ✅ GET route - no auth needed (public)
 router.get("/:id", getAssignmentController);
 
 export default router;
