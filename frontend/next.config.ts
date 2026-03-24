11f6264fa81f21d1b6716bdf64063acf0ed3b1c4
Nikhil Gupta <100pranjalgupta@gmail.com>
2026-03-24 21:39:31 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,39 +1,3 @@
-// // import type { NextConfig } from "next";
-
-// // const nextConfig: NextConfig = {
-// //   /* config options here */
-// // };
-
-// // export default nextConfig;
-
-
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   async headers() {
-//     return [
-//       {
-//         source: "/(.*)",
-//         headers: [
-//           {
-//             key: "Cross-Origin-Opener-Policy",
-//             value: "same-origin-allow-popups",
-//           },
-//           {
-//             key: "Cross-Origin-Embedder-Policy",
-//             value: "unsafe-none",
-//           },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
