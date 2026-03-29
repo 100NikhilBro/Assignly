8f90697e19cda9412c3971d9d20c4cc7e5495c17
Nikhil Gupta <100pranjalgupta@gmail.com>
2026-03-30 01:17:05 +0530

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get("token")?.value;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ["/create-assignment"],
-};
+// export const config = {
+//   matcher: ["/create-assignment"],
+// };
